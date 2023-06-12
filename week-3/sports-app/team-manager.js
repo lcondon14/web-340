08008b97d04df5d200fb8a15af35112638289a66
lcondon14 <122494018+lcondon14@users.noreply.github.com>
2023-06-11 20:12:18 -0500

    add week-3

---
+++ week-3/sports-app/team-manager.js
@@ -0,0 +1,38 @@
+/** 
+ * Title: team-manager.js
+ * Author: Laurel Condon
+ * Date: 11 June 2023
+ * Description: team.js file
+ */
+
+"use strict";
+
+// Team module imported
+const Team = require("./team");
+
+// Array for teams 
+const teams = [
+    new Team("Atlanta Braves", "Blooper", "25"),
+    new Team("Colorado Rockies", "Dinger", "25"),
+    new Team("LA Dodgers", "n/a", "25"),
+    new Team("Houston Astros","Orbit", "25"),
+    new Team("Miami Marlins", "Billy the Marlin", "25")
+];
+
+function getTeams() {
+    return teams;
+}
+
+function getTeam(name) {
+    return teams.find(team => team.name === name);
+}
+
+function displayTeam(team) {
+    return 'Name: ${team.name}\nMascot: ${team.mascot}\nPlayer Count: ${team.playerCount}';
+}
+
+module.exports = {
+    getTeam,
+    getTeams,
+    displayTeam
+};
---
+++ week-3/sports-app/team.js
@@ -0,0 +1,15 @@
+/** 
+ * Title: team.js
+ * Author: Laurel Condon
+ * Date: 11 June 2023
+ * Description: team.js file
+ */
+
+export class Team {
+    constructor(name, mascot, playerCount){
+        this.name = name;
+        this.mascot = mascot;
+        this.playerCount = playerCount;
+    }
+}
+
